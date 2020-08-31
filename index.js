140824eee447df23da5bee82f6c7515c4e9ae916
technologicalsouls <technologicalsouls@gmail.com>
2020-08-30 20:51:02 -0400

    create repository class, methods from users class

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 // CLASSES
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
 // MIDDLEWARE
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cookieSession({
@@ -14,6 +16,7 @@ app.use(
     })
 );
 app.use(authRouter);
+app.use(productsRouter);
 
 //PORT
 app.listen(3003, () => {
